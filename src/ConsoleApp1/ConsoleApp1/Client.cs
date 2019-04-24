9ce62fb68a2b14dc656966e83f5284b700f0d653
Artem-Marakhovskyi <Artem.marakhovskyi@gmail.com>
2019-04-24 12:41:10 +0000

    Finally completed

---
+++ src/ConsoleApp1/ConsoleApp1/Client.cs
@@ -0,0 +1,15 @@
+﻿using Navigation;
+
+namespace NavigationClient
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            DirectionProvider d = new DirectionProvider();
+
+            System.Console.WriteLine(d.GetDirection(0));
+            System.Console.ReadLine();
+        }
+    }
+}
