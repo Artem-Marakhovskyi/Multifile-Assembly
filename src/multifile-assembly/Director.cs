2cbf284210d2d37fe8a905d84ec9d6eb6625cd13
Artem-Marakhovskyi <Artem.marakhovskyi@gmail.com>
2019-04-24 06:07:12 +0000

    -Two .cs files are added, module + exe is created

---
+++ src/multifile-assembly/Director.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Navigation
+{
+    public class Director
+    {
+        public int _direction = 0;
+
+        public string GetDirection()
+        {
+            if (_direction == 0)
+            {
+                return "North";
+            }
+            else if (_direction == 1)
+            {
+                return "East";
+            }
+            else if (_direction == 2)
+            {
+                return "South";
+            }
+            else if (_direction == 3)
+            {
+                return "West";
+            }
+
+            throw new InvalidOperationException("Invalid state");
+        }
+
+        public void TurnRight()
+        {
+            _direction = (_direction + 1) % 4;
+        }
+
+        public void TurnLeft()
+        {
+            _direction = (_direction + 3) % 4;
+        }
+    }
+}
---
+++ src/multifile-assembly/Program.cs
@@ -0,0 +1,24 @@
+﻿using Navigation;
+
+namespace NavigationClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var director = new Director();
+            System.Console.WriteLine(director.GetDirection());
+            director.TurnLeft();
+            director.TurnLeft();
+            System.Console.WriteLine(director.GetDirection());
+            director.TurnLeft();
+            System.Console.WriteLine(director.GetDirection());
+            director.TurnRight();
+            director.TurnRight();
+            director.TurnRight();
+            director.TurnRight();
+            System.Console.WriteLine(director.GetDirection());
+            System.Console.ReadLine();
+        }
+    }
+}
