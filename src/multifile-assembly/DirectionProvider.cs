478b83733f60ee310644b90c9a51efd425365efd
Artem-Marakhovskyi <Artem.marakhovskyi@gmail.com>
2019-04-24 06:25:08 +0000

    reworked

---
+++ src/multifile-assembly/DirectionProvider.cs
@@ -0,0 +1,24 @@
+﻿namespace Navigation
+{
+    public class DirectionProvider
+    {
+        private string[] _directions =
+            new string[]
+            {
+                "North",
+                "East",
+                "South",
+                "West"
+            };
+
+        public string GetDirection(int direction)
+        {
+            if (direction < 4 && direction >= 0)
+            {
+                return _directions[direction];
+            }
+
+            return "Invalid direction";
+        }
+    }
+}
